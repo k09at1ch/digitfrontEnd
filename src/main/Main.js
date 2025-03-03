24a8566bce3f9ebac53786a7f9c4065e4e491c0f
k09at1ch <newwerasik228@gmail.com>
2025-03-03 08:32:14 +0200

    some changes

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { ScaleLoader } from "react-spinners";
-
+import MainCard from "mainCard/MainCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function Main() {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        
+    }, [])
     return (
       <div>
-        <ScaleLoader color="#00ff20" speedMultiplier="2" />
+            <ScaleLoader color="#00ff20" speedMultiplier="2" />
+            
+
+
       </div>
     );
 }
---
+++ src/mainCard/MainCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainCard() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MainCard
